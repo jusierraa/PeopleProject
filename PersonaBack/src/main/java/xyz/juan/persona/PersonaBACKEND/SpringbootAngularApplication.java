e367c92561bdf6eaefb4e75b052a0fbb2498082b
Juan Camilo Sierra Alean <80333208+jusierraa@users.noreply.github.com>
2024-04-01 11:22:58 -0500

    Add files via upload

---
+++ PersonaBack/src/main/java/xyz/juan/persona/PersonaBACKEND/SpringbootAngularApplication.java
@@ -0,0 +1,13 @@
+package xyz.juan.persona.PersonaBACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularApplication.class, args);
+	}
+
+}
---
+++ PersonaBack/src/test/java/xyz/juan/persona/PersonaBACKEND/SpringbootAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.juan.persona.PersonaBACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
